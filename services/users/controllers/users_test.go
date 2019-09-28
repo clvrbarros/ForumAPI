67e964af0015d3a89e96149ee093414973f3166f
clvrbarros <cleversonferreirab@gmail.com>
2019-09-28 14:23:55 -0300

    add test for CreateUser

---
+++ services/users/controllers/users_test.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCreateUser(t *testing.T) {
+	//body := gin.H{
+	//	"email": "email@gmail.com",
+	//	"firstname": "name",
+	//	"password": "123456",
+	//}
+	r := gin.Default()
+	req, _  := http.NewRequest("POST","/register", nil)
+
+	w := httptest.NewRecorder()
+	r.Handle("POST", "/register", CreateUser)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
