74caabe9d264ade151190fa1ea8af57d6fb13b55
clvrbarros <cleversonferreirab@gmail.com>
2019-09-28 01:25:11 -0300

    main inits refactored

--- services/users/main.go
+++ services/users/main.go
@@ -4,11 +4,24 @@ import (
 	"github.com/clvrbarros/ForumAPI/services/users/controllers"
 	"github.com/clvrbarros/ForumAPI/services/users/models"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func main() {
-	defer models.Database.Close()
+	controllers.InitValidator()
+	err := models.InitDb()
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Println("PostgresSQL: Connection OK")
+	defer models.CloseDb()
+
 	r := gin.Default()
+
+	// Routes
 	r.POST("/register", controllers.CreateUser)
+	r.POST("/login", controllers.CreateUser)
+	r.POST("/register2", controllers.CreateUser)
+
 	r.Run()
 }
