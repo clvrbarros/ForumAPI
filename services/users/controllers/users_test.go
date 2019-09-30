aba128785ea4343cc701f75d7545190ff1419da7
clvrbarros <cleversonferreirab@gmail.com>
2019-09-30 14:20:35 -0300

    update users tests

--- services/users/controllers/users_test.go
+++ services/users/controllers/users_test.go
@@ -1,25 +1,81 @@
 package controllers
 
 import (
-	"github.com/gin-gonic/gin"
+	"bytes"
+	"encoding/json"
+	"github.com/clvrbarros/ForumAPI/services/users/helper"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestCreateUser(t *testing.T) {
-	//body := gin.H{
-	//	"email": "email@gmail.com",
-	//	"firstname": "name",
-	//	"password": "123456",
-	//}
-	r := gin.New()
-	req, _  := http.NewRequest("POST","/register", nil)
+	const (
+		method = "POST"
+		path = "/register"
+	)
+	router.Handle(method, path, h.CreateUser)
 
-	w := httptest.NewRecorder()
-	r.Handle("POST", "/register", CreateUser)
-	r.ServeHTTP(w, req)
+	t.Run("Should return 201 when successful", func(t *testing.T) {
+		var newUser = []byte(`{
+				"email": "teste@gmail.com",
+				"firstname": "Test",
+				"password": "123456"
+				}`)
 
-	assert.Equal(t, http.StatusBadRequest, w.Code)
+		req, rr := request(method, path, bytes.NewBuffer(newUser), t)
+		router.ServeHTTP(rr, req)
+
+		assert.Equal(t, http.StatusCreated, rr.Code)
+	})
+
+	t.Run("Should return 400 and error message", func(t *testing.T) {
+		t.Run("when body is empty", func(t *testing.T) {
+			req, rr := request(method, path, nil, t)
+			router.ServeHTTP(rr, req)
+
+			assert.Equal(t, http.StatusBadRequest, rr.Code)
+		})
+		t.Run("when email is not valid", func(t *testing.T) {
+			var newUser = []byte(`{
+				"email": "testegmail.com",
+				"firstname": "Test",
+				"password": "1236"
+				}`)
+			var response helper.APIValidator
+
+			req, rr := request(method, path, bytes.NewBuffer(newUser), t)
+			router.ServeHTTP(rr, req)
+
+			readBuf, _ := ioutil.ReadAll(rr.Body)
+			err := json.Unmarshal(readBuf, &response)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			assert.Equal(t,"The email field is not valid", response.Errors[0].Message)
+			assert.Equal(t, http.StatusBadRequest, rr.Code)
+		})
+		t.Run("when firstname is empty", func(t *testing.T) {
+			var newUser = []byte(`{
+				"email": "teste@gmail.com",
+				"firstname": "",
+				"password": "123645"
+				}`)
+			var response helper.APIValidator
+
+			req, rr := request(method, path, bytes.NewBuffer(newUser), t)
+			router.ServeHTTP(rr, req)
+
+			readBuf, _ := ioutil.ReadAll(rr.Body)
+			err := json.Unmarshal(readBuf, &response)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			assert.Equal(t,"The firstname field is required", response.Errors[0].Message)
+			assert.Equal(t, http.StatusBadRequest, rr.Code)
+		})
+	})
 }
