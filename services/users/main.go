c26d9f0168dbf38f5a80bb363a0e553499f113f7
clvrbarros <cleversonferreirab@gmail.com>
2019-09-30 09:54:33 -0300

    all instances init in main

--- services/users/main.go
+++ services/users/main.go
@@ -1,21 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/clvrbarros/ForumAPI/services/users/controllers"
 	"github.com/clvrbarros/ForumAPI/services/users/models"
 	"github.com/clvrbarros/ForumAPI/services/users/router"
+	_ "github.com/lib/pq"
+	"gopkg.in/go-playground/validator.v9"
 	"log"
 )
 
-func main() {
-	controllers.InitValidator()    // Init validator instance
-	err := models.InitDb()		   // Init db instance
-	if err != nil {
-		log.Panic(err)
-	}
-	log.Println("PostgresSQL: Connection OK")
-	defer models.CloseDb()
+var v *validator.Validate
 
-	r := router.NewRouter()			// Load routes
+func main() {
+	databaseInstance := database("user=postgres dbname=forum " +
+		"password=admin host=127.0.0.1 sslmode=disable")
+	modelInstance := models.Connect(databaseInstance)
+	validatorInstance := validator.New()
+	controllerInstance := controllers.NewController(modelInstance, validatorInstance)
+
+	defer databaseInstance.Close()
+
+	r := router.NewRouter(controllerInstance)
 	r.Run()
+}
+
+func database(connect string) *sql.DB {
+	db, err := sql.Open("postgres", connect)
+	if err != nil {
+		log.Fatalln("Could not open connection to Postgres: ", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalln("Could not connect to MySQL: ", err)
+	}
+
+	log.Println("Database connected")
+	return db
 }
